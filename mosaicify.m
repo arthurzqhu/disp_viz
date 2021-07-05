8deb8fcda0b56b2fae720695b789458503572feb
Arthur <ols.Arthur@gmail.com>
2021-07-05 00:30:54 -0700

    compatible with more data structures

--- mosaicify.m
+++ mosaicify.m
@@ -1,16 +1,21 @@
 function avgd=mosaicify(raw, rat)
 % convert raw data into segment-averaged data
 
-nraw=length(raw);
+if rat==1
+   avgd=raw;
+   return
+end
+
+nraw=max(size(raw)); % the longer dimension is the length by default
 navg=floor(nraw/rat);
 
-avgd=zeros(navg,1);
+avgd=zeros(navg,min(size(raw)));
 
 for iavg=1:navg
    idxs=(iavg-1)*rat+1;
    idxe=iavg*rat;
    
-   avgd(iavg)=nanmean(raw(idxs:idxe));
+   avgd(iavg,:)=nanmean(raw(idxs:idxe,:));
 end
 
 end
