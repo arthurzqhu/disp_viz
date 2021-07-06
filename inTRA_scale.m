2dc8f9a51b38fe5543403c3dd46ff8e6d2614c75
Arthur <ols.Arthur@gmail.com>
2021-07-06 00:42:36 -0700

    post is now less top-biased (2x max)

--- inTRA_scale.m
+++ inTRA_scale.m
@@ -9,26 +9,34 @@ drp_instr={'pdi','pdi','pdi','pdi','pdi'};
 dpc='#FB9966'; % datapoint color
 % axis_color='#336C8A';
 nc=length(campaigns);
+colororder=colororder;
+color_order={};
+for i=1:size(colororder,1)
+    color_order{i}=colororder(i,:);
+end
 %% inflight averaging and cloud averaging (without subsampling)
 close all
 clear X avgcoeff avgtest_rsq avgtrain_rsq
 
-arat=[1 100 300 800]; % averaging ratio
+arat=2.^(0:.5:10);%[1:9 10:10:90 100:100:1000]; % averaging ratio
 falp=(erf(log10(arat)-1)+1)/2;
 ntrial=20;
 [Sctest_rsq,Sctrain_rsq]=deal(zeros(length(arat),ntrial));
 Sccoeff={};
+max_ratio=[1 2 3 5 10 20 50];
+iratio=2;
+nlyrs=20;
 
 tic
-for itrial=1%:ntrial
-%    itrial
+for itrial=1:ntrial
+   itrial
    for ir=1:length(arat)
 
-      figure('Position',[1032 199 1549 778]);
-      tl=tiledlayout('flow');
+%       figure('Position',[1032 199 1549 778]);
+%       tl=tiledlayout('flow');
       [allSc_YYss,allSc_XXss]=deal([]);
 
-      for c=1:nc
+      for c=1:3
          [s_lwc_pdi,normAC,s_ntot_aer,s_ntot_pdi,s_ntot_pcasp,ql_adb,rhoa,...
             s_actfrac_imp,reldisp,reldisp_imp]=deal([]);
 
@@ -93,6 +101,29 @@ for itrial=1%:ntrial
             reldisp_imp=[reldisp_imp;...
                mosaicify(clouds.(camp)(iday).s_disp_pdi,arat(ir))];
          end
+         
+         if strcmp(camp,'postpdi')
+            % unbias the vertical sampling
+            normAC_incloud=normAC>=0&normAC<=1;
+            [N_raw,edges,lyrs]=histcounts(normAC(normAC_incloud),nlyrs);
+            minNval=min(N_raw(N_raw>0));
+            N_raw(N_raw>max_ratio(iratio)*minNval)=...
+                           max_ratio(iratio)*minNval;
+            rdsmp_idx=[];
+            for ilyr=1:nlyrs
+               rdsmp_idx_lyr=randsample(find(lyrs==ilyr),N_raw(ilyr));
+               rdsmp_idx=[rdsmp_idx;rdsmp_idx_lyr];
+            end
+            
+            s_lwc_pdi=s_lwc_pdi(rdsmp_idx);
+            normAC=normAC(rdsmp_idx);
+            rhoa=rhoa(rdsmp_idx);
+            ql_adb=ql_adb(rdsmp_idx);
+            reldisp=reldisp(rdsmp_idx);
+            s_ntot_pdi=s_ntot_pdi(rdsmp_idx);
+            s_ntot_aer=s_ntot_aer(rdsmp_idx);
+            
+         end
 
          s_actfrac=s_ntot_pdi./s_ntot_aer;
          ql_obs=s_lwc_pdi./rhoa;
@@ -116,28 +147,28 @@ for itrial=1%:ntrial
 
          yy=reldisp(vidx);
 
-         [avgcoeff{ir}(c,:), avgtrain_rsq{ir}(c), avgtest_rsq{ir}(c)]=...
-            regress_tt(yy, X, .7);
+%          [avgcoeff{ir}(c,:), avgtrain_rsq{ir}(c), avgtest_rsq{ir}(c)]=...
+%             regress_tt(yy, X, .7);
 
-         yhat=X*avgcoeff{ir}(c,:)';
+%          yhat=X*avgcoeff{ir}(c,:)';
 
-         nexttile
-         scatter(yhat,yy,'filled',...
-            'MarkerFaceAlpha',falp(ir),'MarkerFaceColor',dpc)
-         title(ctitle{c},'FontSize',16)
-         refline(1,0)
-         ylim([0 1])
-         xlim([0 1])
+%          nexttile
+%          scatter(yhat,yy,'filled',...
+%             'MarkerFaceAlpha',falp(ir),'MarkerFaceColor',dpc)
+%          title(ctitle{c},'FontSize',16)
+%          refline(1,0)
+%          ylim([0 1])
+%          xlim([0 1])
          
-         vidx=~isnan(yhat+yy);
+%          vidx=~isnan(yhat+yy);
    %       rsq=1-sum((yhat(vidx)-yy(vidx)).^2)/sum((yy(vidx)-mean(yy(vidx))).^2);
    %       rsq=avgtest_rsq{ir}(c);
          
-         pred_str=['\Pi_C','=', sprintf('%0.3f', avgtest_rsq{ir}(c))];
-         axisdim=axis;
-         textx=(axisdim(2)-axisdim(1))*0.7+axisdim(1);
-         texty=(axisdim(4)-axisdim(3))*0.6+axisdim(3);
-         text(textx,texty,pred_str,'FontSize',16)
+%          pred_str=['\Pi_C','=', sprintf('%0.3f', avgtest_rsq{ir}(c))];
+%          axisdim=axis;
+%          textx=(axisdim(2)-axisdim(1))*0.7+axisdim(1);
+%          texty=(axisdim(4)-axisdim(3))*0.6+axisdim(3);
+%          text(textx,texty,pred_str,'FontSize',16)
 
          if ismember(c,1:3)
             XXSc_cell{c}=X;
@@ -161,61 +192,83 @@ for itrial=1%:ntrial
       [Sccoeff{ir,itrial},Sctrain_rsq(ir,itrial),Sctest_rsq(ir,itrial)]=...
             regress_tt(allSc_YYss, allSc_XXss, .7);
 
-      yhatSc=allSc_XXss*Sccoeff{ir,itrial};
-      nexttile
-      scatter(yhatSc,allSc_YYss,'filled',...
-         'MarkerFaceAlpha',falp(ir),'MarkerFaceColor',dpc)
-      title('All Sc','FontSize',16)
-      refline(1,0)
-      ylim([0 1])
-      xlim([0 1])
+%       yhatSc=allSc_XXss*Sccoeff{ir,itrial};
+%       nexttile
+%       scatter(yhatSc,allSc_YYss,'filled',...
+%          'MarkerFaceAlpha',falp(ir),'MarkerFaceColor',dpc)
+%       title('All Sc','FontSize',16)
+%       refline(1,0)
+%       ylim([0 1])
+%       xlim([0 1])
       
-      vidx=~isnan(yhatSc+allSc_YYss);
+%       vidx=~isnan(yhatSc+allSc_YYss);
    %    rsq=1-sum((yhatSc(vidx)-allSc_YYss(vidx)).^2)/sum((allSc_YYss(vidx)-mean(allSc_YYss(vidx))).^2);
-      rsq=Sctest_rsq(ir);
-      pred_str=['\Pi_C','=', sprintf('%0.3f',rsq)];
-      axisdim=axis;
-      textx=(axisdim(2)-axisdim(1))*0.7+axisdim(1);
-      texty=(axisdim(4)-axisdim(3))*0.6+axisdim(3);
-      text(textx,texty,pred_str,'FontSize',16)
-      
-      title(tl,['ratio=' num2str(arat(ir))],'fontsize',20,'fontweight','bold')
+%       rsq=Sctest_rsq(ir);
+%       pred_str=['\Pi_C','=', sprintf('%0.3f',rsq)];
+%       axisdim=axis;
+%       textx=(axisdim(2)-axisdim(1))*0.7+axisdim(1);
+%       texty=(axisdim(4)-axisdim(3))*0.6+axisdim(3);
+%       text(textx,texty,pred_str,'FontSize',16)
+%       
+%       title(tl,['ratio=' num2str(arat(ir))],'fontsize',20,'fontweight','bold')
 %       saveas(gcf,['plots/scale dep test rsq ratio=' num2str(arat(ir)) ' proper.png'])
 
    end
-   toc
 end
 toc
 %%
+close all
+% load scale_analysis
+figure('Position',[1282 951 1142 386])
+tl=tiledlayout('flow');
+
 rsq_mean=mean(Sctest_rsq,2);
-rsq_std=std(Sctest_rsq');
+rsq_std=std(Sctest_rsq')';
 
 % errorbar(arat,rsq_mean,rsq_std,'LineWidth',2)
-plot(arat*55/1e3,rsq_mean,'LineWidth',2)
+nexttile
+hold on
+scalex=arat'*55/1e3;
+
+fill([scalex;flipud(scalex)],...
+   [rsq_mean-rsq_std;flipud(rsq_mean+rsq_std)],...
+   color_order{1},'FaceAlpha',0.2,'LineStyle','none')
+      
+plot(scalex,rsq_mean,'LineWidth',2,'Color',color_order{1})
 grid
 set(gca,'XScale','log')
-xlabel('scale [km]')
 ylabel('\Pi_{Sc}')
 set(gca,'FontSize',16)
-saveas(gcf,'plots/pi vs scale.png')
+% saveas(gcf,'plots/pi vs scale.png')
 
-%%
-coeff_itr=zeros(ntrial,size(X,2));
-coeff_ir=zeros(length(arat),size(X,2));
+coeff_itr=zeros(ntrial,size(Sccoeff{1,1},1));
+coeff_mu=zeros(length(arat),size(Sccoeff{1,1},1));
+coeff_std=zeros(length(arat),size(Sccoeff{1,1},1));
+
 for ir=1:length(arat)
 
    for itrial=1:ntrial
    coeff_itr(itrial,:)=Sccoeff{ir,itrial};
    end
 
-   coeff_ir(ir,:)=mean(coeff_itr);
+   coeff_mu(ir,:)=mean(coeff_itr);
+   coeff_std(ir,:)=std(coeff_itr);
+end
+nexttile
+hold on
+plot(scalex,coeff_mu,'LineWidth',2)
+for i=1:3
+   fill([scalex;flipud(scalex)],...
+      [coeff_mu(:,i)-coeff_std(:,i);flipud(coeff_mu(:,i)+coeff_std(:,i))],...
+      color_order{i},'FaceAlpha',0.2,'LineStyle','none')
 end
 
-plot(arat*55/1e3,coeff_ir,'LineWidth',2)
+
+
 grid
 set(gca,'XScale','log')
-legend('b','m_{LAF}','m_{NH}','Location','best')
+legend('Int','m_{LAF}','m_{NH}','Location','best')
+xlabel(tl,'scale [km]','fontsize',20)
+ylabel('Coefficients')
 set(gca,'FontSize',16)
-xlabel('scale [km]')
-ylabel('coeff.')
-saveas(gcf,'plots/coeff vs scale.png')
+saveas(gcf,'plots/pi & coeff vs scale.png')
